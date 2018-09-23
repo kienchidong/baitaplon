9a8633600a1b2f258a1b0119c9ba29dc0723242a
kienc <kienchidong>
2018-09-23 17:00:15 +0700

    unit

---
+++ production/define.php
@@ -0,0 +1,4 @@
+<?php
+define("SITE_URL", "http://localhost:8080/1807E/production/");
+define("ADMIN_PATH", dirname(__FILE__).'/');
+?>
---
+++ production/index.php
@@ -0,0 +1,33 @@
+<?php require 'define.php'?>
+<?php require ADMIN_PATH.'partial/header.php';
+
+?>
+<div class="page-container">
+    <div class="left-content">
+        <div class="mother-grid-inner">
+            <?php require ADMIN_PATH.'partial/header-main.php'; ?>
+            <?php
+            if(isset($_GET['page'])) {
+                $page=$_GET['page'].'.php';
+                $page_path = ADMIN_PATH.'pages/'. $page;
+                $check_file_path = file_exists($page_path);
+                if($check_file_path){
+                    require $page_path;
+                }
+                else{
+                    require 'pages/404.php';
+                }
+            }
+            else{
+                require 'pages/dashbroad.php';
+            }
+            ?>
+            <?php require ADMIN_PATH.'partial/copyright.php';?>
+        </div>
+    </div>
+    <?php require ADMIN_PATH.'partial/sidebar.php';?>
+    <div class="clearfix"> </div>
+</div>
+<!--slide bar menu end here-->
+<!-- mother grid end here-->
+<?php require ADMIN_PATH.'partial/footer.php'?>
